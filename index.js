0fea83400d464cb77311b0d0b80f55e2bcc406e0
lcy <666399@qq.com>
2023-02-19 16:14:32 +0800

    Revert "返回55版本"
    This reverts commit 55941ddd8395f2d99aecc888b8abd5cfa5d6461b.

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
  * @Author: lcy
  * @Date: 2023-02-19 12:06:29
  * @LastEditors: lcy
- * @LastEditTime: 2023-02-19 14:57:57
+ * @LastEditTime: 2023-02-19 14:46:52
  */
 function test() {
   console.log("RES");
   console.log(11111);
   console.log(44444);
-  console.log(55555);
 }
