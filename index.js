55941ddd8395f2d99aecc888b8abd5cfa5d6461b
lcy <666399@qq.com>
2023-02-19 15:00:58 +0800

    555版本

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
  * @Author: lcy
  * @Date: 2023-02-19 12:06:29
  * @LastEditors: lcy
- * @LastEditTime: 2023-02-19 14:46:52
+ * @LastEditTime: 2023-02-19 14:57:57
  */
 function test() {
   console.log("RES");
   console.log(11111);
   console.log(44444);
+  console.log(55555);
 }
