58b375b787930bc292c75d40cc8676c9c1f96711
lcy <666399@qq.com>
2023-02-19 14:47:07 +0800

    3333

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  * @Author: lcy
  * @Date: 2023-02-19 12:06:29
  * @LastEditors: lcy
- * @LastEditTime: 2023-02-19 14:44:34
+ * @LastEditTime: 2023-02-19 14:46:52
  */
 function test() {
   console.log("RES");
+  console.log(11111);
 }
